0753b5e10d8e210bed1af9cc438c9e6f6b4ec535
EnergieNVidia77 <59421930+EnergieNVidia77@users.noreply.github.com>
2020-01-04 23:07:03 +0100

    Add files via upload

--- LoadSavePNG.java
+++ LoadSavePNG.java
@@ -1,4 +1,5 @@
 import javax.imageio.ImageIO;
+
 import java.awt.image.BufferedImage;
 import java.awt.Color;
 import java.io.File;
@@ -10,17 +11,9 @@ public class LoadSavePNG
 {
 	public static int K;
 	public static int D;
-
-	public static double[][] initialiseM(int K, int D) {
-		double[][] m = new double[K][D];
-		m[0] = new double[]{231/255.0, 221/255.0, 62/255.0};
-		m[1] = new double[]{28/255.0, 113/255.0, 193/255.0};
-		m[2] = new double[]{98/255.0, 228/255.0, 84/255.0};
-		m[3] = new double[]{231/255.0, 117/255.0, 52/255.0};
-		m[4] = new double[]{44/255.0, 51/255.0, 41/255.0};
-		m[5] = new double[]{173/255.0, 23/255.0, 39/255.0};
-		return m;
-	}
+	
+	static String path = "C:\\Users\\Toufic\\Documents\\M&Ms projet\\";
+    static String imageMMS = path + "mms.png";
 
 	public static boolean checkClass(double[] r, int idx) {
 		int highestIdx = 0;
@@ -36,78 +29,39 @@ public class LoadSavePNG
 
 	public static void main(String[] args) throws IOException
 	{
-		String path = System.getProperty("user.dir") + "/src/";
-		String imageMMS = path + "mms.png";
 
 		// Lecture de l'image ici
 		BufferedImage bui = ImageIO.read(new File(imageMMS));
 
 		int width = bui.getWidth();
 		int height = bui.getHeight();
-		System.out.println("Hauteur=" + width);
-		System.out.println("Largeur=" + height);
-
-		int pixel = bui.getRGB(0, 0);
-		//System.out.println("Pixel 0,0 = "+pixel);
-		Color c = new Color(pixel);
-		System.out.println("RGB = "+c.getRed()+" "+c.getGreen()+" "+c.getBlue());
-		// Calcul des trois composant de couleurs normalisé à 1
-		double[] pix = new double[3];
-		pix[0] = (double) c.getRed()/255.0;
-		pix[1] = (double) c.getGreen()/255.0;
-		pix[2] = (double) c.getBlue()/255.0;
-		System.out.println("RGB normalisé= "+pix[0]+" "+pix[1]+" "+pix[2] + "\n");
 
 		int[] im_pixels = bui.getRGB(0, 0, width, height, null, 0, width);
 
 		/** Creation du tableau **/
-		Color[] tabColor= new Color[im_pixels.length];
-		for(int i=0 ; i<im_pixels.length ; i++)
-			tabColor[i]=new Color(im_pixels[i]);
-
-		/** inversion des couleurs **/
-		for(int i=0 ; i<tabColor.length ; i++)
-			tabColor[i]=new Color(255-tabColor[i].getRed(),255-tabColor[i].getGreen(),255-tabColor[i].getBlue());
-
-		/** sauvegarde de l'image **/
-		BufferedImage bui_out = new BufferedImage(bui.getWidth(),bui.getHeight(),BufferedImage.TYPE_3BYTE_BGR);
-		for(int i=0 ; i<height ; i++)
-		{
-			for(int j=0 ; j<width ; j++)
-				bui_out.setRGB(j,i,tabColor[i*width+j].getRGB());
-		}
-		ImageIO.write(bui_out, "PNG", new File(path+"test.png"));
-
-		int K = 6;
-		int D = 3;
+		System.out.println("Creation of temporary database");
+		 
+		 double[][] data = MixGauss.genData(im_pixels);
+		 double[][] centres = MixGauss.genCenter();
+		 
+		double[][] var = MixGauss.InitVariance();
+		double[] dens = MixGauss.InitDensite();
+		
+		double[][] r = new double[data.length][centres.length];
+		
 		double eps=0.001;
 		double maj = 10;
-		double[][] x = new double[width*height][D];
-		double[][] m = new double[K][D];
-		double[][] r = new double[K][D];
-
-		for (int i = 0; i < height; i++) {
-			for (int j = 0; j < width; j++) {
-				int idx = i*width + j;
-				x[idx][0] = tabColor[idx].getRed()/255.0;
-				x[idx][1] = tabColor[idx].getBlue()/255.0;
-				x[idx][2] = tabColor[idx].getGreen()/255.0;
-			}
-		}
-		m = initialiseM(K, D);
-		MixGauss.setup(K, D);
-
-		System.out.println("Pos ctr1: " + Arrays.toString(m[1]) + "\nPos ctr2: " + Arrays.toString(m[2]) + "\n");
 		int nbEpoch = 0;
 		while(maj > eps) {
 			System.out.println("Epoque #" + nbEpoch);
-			r = MixGauss.Assign(x, m);
-			maj = MixGauss.Deplct(x, m, r);
+			r = MixGauss.Assigner(data, centres, var, dens);
+			maj = MixGauss.Deplct(data, centres, r, var, dens);
+			System.out.println(maj);
 			nbEpoch++;
 		}
 		System.out.println("\nNb epoques: " + nbEpoch + "\n");
 		System.out.println("Proba pt1: " + Arrays.toString(r[1]) + "\nProba pt4000: " + Arrays.toString(r[4000]));
-		System.out.println("Pos ctr1: " + Arrays.toString(m[1]) + "\nPos ctr2: " + Arrays.toString(m[2]) + "\n");
+		System.out.println("Pos ctr1: " + Arrays.toString(centres[1]) + "\nPos ctr2: " + Arrays.toString(centres[2]) + "\n");
 
 		BufferedImage res = new BufferedImage(bui.getWidth(), bui.getHeight(), BufferedImage.TYPE_INT_RGB);
 		for (int i = 0; i < height; i++) {
--- MixGauss.java
+++ MixGauss.java
@@ -1,24 +1,82 @@
+import java.awt.Color;
 import java.util.Arrays;
 
 public class MixGauss {
-	static int D; // deux dimensions
-	static int K; // trois centres
-	static double[][] variance;
-	static double[] rho;
+	static int D = 3; // deux dimensions
+	static int K = 8; // trois centres
+	
+	public static double[][] genData(int[] tabPix) {
+		double[][] result = new double[tabPix.length][D];
+		for(int i = 0; i < tabPix.length; i++) {
+			Color c = new Color(tabPix[i]);
+			result[i][0] = (double) c.getRed()/255;
+			result[i][1] = (double) c.getGreen()/255;
+			result[i][2] = (double) c.getBlue()/255;
+		}
+		return result;
+	}
+	public static double[][] genCenter(){
+		double[][] result = new double[K][D];
+		//fond inversé
+        result[0][0] = 59.0/255.0;
+        result[0][1] = 78.0/255.0;
+        result[0][2] = 147.0/255.0;
+        
+        //reflet inversé
+        result[1][0] = 98.0/255.0;
+        result[1][1] = 95.0/255.0;
+        result[1][2] = 102.0/255.0;
+        
+        //jaune inversé
+        result[2][0] = 32.0/255.0;
+        result[2][1] = 35.0/255.0;
+        result[2][2] = 255.0/255.0;
+        
+        //vert inversé
+        result[3][0] = 216.0/255.0;
+        result[3][1] = 48.0/255.0;
+        result[3][2] = 206.0/255.0;
+        
+        //bleu inversé
+        result[4][0] = 255.0/255.0;
+        result[4][1] = 151.0/255.0;
+        result[4][2] = 96.0/255.0;
+        
+        //rouge inversé
+        result[5][0] = 90.0/255.0;
+        result[5][1] = 206.0/255.0;
+        result[5][2] = 221.0/255.0;
+        
+        //orange inversé
+        result[6][0] = 4.0/255.0;
+        result[6][1] = 160.0/255.0;
+        result[6][2] = 245.0/255.0;
+        
+        //noir inversé
+        result[7][0] = 196.0/255.0;
+        result[7][1] = 207.0/255.0;
+        result[7][2] = 229.0/255.0;
+        
+		return result;
+	}
 
-
-	public static void setup(int k, int d) {
-		K = k;
-		D = d;
-		variance = new double[K][D];
-		rho = new double[K];
-
-		for (int i = 0; i < variance.length; i++) {
-			rho[i] = 1.0/K;
-			for (int j = 0; j < variance[i].length; j++) {
-				variance[i][j] = 0.00010;
+	
+	public static double[][] InitVariance() {
+		double[][] var = new double[K][D];
+		for(int i = 0; i < var.length; i++) {
+			for(int j = 0; j < var[i].length; j++) {
+				var[i][j] = 0.50;
 			}
 		}
+		return var;
+	}
+	
+	public static double[] InitDensite() {
+		double[] p = new double[K];
+        for(int i = 0; i<p.length; i++){
+        	p[i] = 1.0/(double) K;
+        }
+        return p;
 	}
 
 	public static double dist(double[] pt1, double[] pt2) {
@@ -28,38 +86,31 @@ public class MixGauss {
 		}
 		return Math.sqrt(distance);
 	}
+	
 
-	public static double calculate(double[] x, double[][] m, int k) {
-		double tot = rho[k];
-		for (int i = 0; i < x.length; i++) {
-			tot *= (1 / Math.sqrt(2 * Math.PI * variance[k][i])) * Math.exp(-(x[i] - m[k][i]) / (2 * variance[k][i]));
-		}
-		return tot;
-	}
-
-	/**
-	 * @param x n-dimension data
-	 * @param m n-dimension centers
-	 * @return Nearest center of each data
-	 */
-	public static double[][] Assign(double[][] x, double[][] m) {
-		double[][] r = new double[x.length][m.length];
-		for (int d = 0; d < r.length; d++) {
-			float tot = 0;
-			for (int k = 0; k < r[d].length; k++) {
-				r[d][k] = calculate(x[d], m, k);
-				tot += r[d][k];
+	public static double[][] Assigner(double[][] data, double[][] center, double[][] variance, double[] densite){
+		double[][] result = new double[data.length][center.length];
+		for(int i = 0; i < result.length; i++) {
+			double sum = 0;
+			for(int k = 0; k < result[i].length; k++) {
+				double dens = densite[k];
+				for(int j = 0; j < data[i].length; j++) {
+					dens = dens*(1/Math.sqrt(2*Math.PI*variance[k][j]));
+					dens = dens*Math.exp(-((data[i][j]-center[k][j])*(data[i][j]-center[k][j]))/(2*variance[k][j]));
+				}
+				sum += dens;
+				result[i][k] = dens;
 			}
-			for (int k = 0; k < r[d].length; k++) {
-				r[d][k] /= tot;
+			for(int k = 0; k < result[i].length; k++) {
+				result[i][k] = result[i][k]/sum;
 			}
 		}
-		return r;
+		return result;
 	}
 
-	public static double[] Rk(double[][] r) {
-		double[] R = new double[r[0].length];
-		for (int k = 0; k < r[0].length; k++) {
+	public static double[] Rk(double[][] r, double[] rho) {
+		double[] R = new double[rho.length];
+		for (int k = 0; k < rho.length; k++) {
 			for (int d = 0; d < r.length; d++) {
 				R[k] += r[d][k];
 			}
@@ -80,7 +131,7 @@ public class MixGauss {
 		return m;
 	}
 
-	public static void updateVar(double[][] r, double[][] x, double[][] m, double[] R) {
+	public static void updateVar(double[][] r, double[][] x, double[][] m, double[] R, double[][] variance) {
 		for (int k = 0; k < variance.length; k++) {
 			for (int i = 0; i < variance[k].length; i++) {
 				double sum = 0;
@@ -92,7 +143,7 @@ public class MixGauss {
 		}
 	}
 
-	public static void updateRho(double[] R, double M) {
+	public static void updateRho(double[] R, double M, double[] rho) {
 		for (int k = 0; k < rho.length; k++) {
 			rho[k] = R[k] / M;
 		}
@@ -104,19 +155,19 @@ public class MixGauss {
 	 * @param r Nearest center index for each data
 	 * @return Total deplacement of the centers
 	 */
-	public static double Deplct(double[][] x, double[][] m, double[][] r) {
+	public static double Deplct(double[][] x, double[][] m, double[][] r, double[][] variance, double[] rho) {
 		double tot = 0;
-		double[] R = Rk(r);
+		double[] R = Rk(r, rho);
 		double[][] oldm = new double[m.length][m[0].length];
-		for (int i = 0; i < oldm.length; i++) {
-			for (int j = 0; j < oldm[i].length; j++) {
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m[i].length; j++) {
 				oldm[i][j] = m[i][j];
 			}
 		}
 
 		m = updateM(r, x, m, R);
-		updateVar(r, x, m, R);
-		updateRho(R, x.length);
+		updateVar(r, x, m, R, variance);
+		updateRho(R, x.length, rho);
 		for (int i = 0; i < m.length; i++) {
 			tot += dist(oldm[i], m[i]);
 		}
